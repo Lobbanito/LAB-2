7f143ee8eadeb525e8072de3efe193c5f50d887c
Elias Robyne <elias.robyne@hotmail.com>
2026-01-04 14:16:45 +0700

    Added popup and bugfixes.

--- contact.js
+++ contact.js
@@ -1,14 +1,21 @@
 "use strict";
 
-/* Fields, variabels fetched from html doc.  */
+/* Fields/elements.  */
 const form = document.querySelector(".contact-form");
 const messageInput = document.getElementById("message");
 const charCounter = document.getElementById("charCounter");
-let firstName = document.getElementById("firstName");
+const popup = document.getElementById("successPopup");
+const popupText = document.getElementById("popupText");
+const overlay = document.getElementById("overlay");
+const clearBtn = document.querySelector('button[type="reset"]');
 
 /* Event listeners */
 messageInput.addEventListener("input", updateCharCounter);  
 form.addEventListener("submit", formSubmit);
+
+clearBtn.addEventListener("click", () => {
+    updateCharCounter();
+})
 
 /* Function for submit button */
 function formSubmit(event) {
@@ -20,6 +27,8 @@ function formSubmit(event) {
     fields.forEach(field => {
         const error = field.parentElement.querySelector(".error-message");
 
+
+        /* If the field is empty. */
         if (field.value.trim() === "")
         {
             showError(field, error, field.dataset.requiredError);
@@ -27,6 +36,7 @@ function formSubmit(event) {
             return;
         }
 
+        /* If the text in the field is invalid */
         if (!isValidField(field))
         {
             showError(field, error, field.dataset.formatError);
@@ -34,16 +44,23 @@ function formSubmit(event) {
             return;
         }
 
+        /* No wrong input, show success */
         clearError(field, error);
     });
 
+    
     if (messageInput.value.length < 20){
         hasErrors = true;
     }
 
     if (!hasErrors){
+        showSuccessPopup();
         form.reset();
         updateCharCounter();
+
+        fields.forEach(field => {
+            field.classList.remove("input-success");
+        })
     }
 }
 
@@ -60,10 +77,19 @@ function isValidField(field){
         return /^[0-9+\s-]+$/.test(field.value);
     }
 
+    if (field.id === "message"){
+        return field.value.trim().length >= 20;
+    }
+
+    if (field.tagName === "SELECT") {
+        return field.value !== "";
+    }
+
     return true; 
 }
 
 
+/* Function for when an field is in error */ 
 function showError(field, errorElement, message){
     errorElement.textContent = message;
     errorElement.style.display = "block";
@@ -72,21 +98,49 @@ function showError(field, errorElement, message){
 
 }
 
+/* Function for when a field is right */
 function clearError(field, errorElement){
     errorElement.textContent = "";
     errorElement.style.display = "none";
     field.classList.remove("input-error");
     field.classList.add("input-success");
 }
+
+/* Updates char counter while typing */
 function updateCharCounter(){
     const length = messageInput.value.length;
-
     charCounter.textContent = `${length} / 20 characters`;
-
-    if (length < 20){
+    if (length === 0){
+        charCounter.style.color = "#999";
+    } else if (length < 20){
         charCounter.style.color = "red";
-        hasErrors = true;
     } else {
         charCounter.style.color = "lightgreen";
     }
+}
+
+function showSuccessPopup(){
+    const firstName = document.getElementById("firstName").value;
+
+    popupText.innerHTML = `Thank you <strong>${firstName}</strong> for reaching out to us, we will be in touch as soon as possible`;
+    popup.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+
+    document.body.classList.add("no-scroll");
+
+
+    setTimeout(closePopup, 3000)
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && !popup.classList.contains("hidden")) {
+        closePopup();
+    }
+});
+
+function closePopup() {
+    popup.classList.add("hidden");
+    overlay.classList.add("hidden");
+
+    document.body.classList.remove("no-scroll");
 }
